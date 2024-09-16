5b2a83023e1b2586fe104654a877b70aca214a85
Himaloy Mondal <himaloymondal100@gmail.com>
2024-09-16 23:26:07 +0530

    basic working done

--- gouribati/views.py
+++ gouribati/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+# from django.urls import reverse
+# from .models import Contact
+# # from django.http import JsonResponse
+# from django.contrib import messages
 
 # Create your views here.
+def index(request):
+    return render(request, "index.html")
+
+def contact(request):
+    return render(request, "contact_us.html")

76bb7c6a833755bc40e35715bb5832d0bcae17bf
Himaloy Mondal <himaloymondal100@gmail.com>
2024-09-16 21:46:18 +0530

    initial setup

--- gouribati/apps.py
+++ gouribati/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AppieConfig(AppConfig):
+class GouribatiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'appie'
+    name = 'gouribati'
